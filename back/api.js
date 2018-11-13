0756bbb611f73c6f432bafa18cdc4a782de00350
cdel2 <cdelfossecsi@gmail.com>
2018-11-13 11:58:30 +0100

    bails cools

--- back/api.js
+++ back/api.js
@@ -1,54 +1,40 @@
-const  GoogleSearch = require('google-search')
-const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+const exec = require('child_process').exec;
+const fs = require('fs');
 
-/*credentials and API settings*/
-//Google API
-const googleSearch = new GoogleSearch({
-    key: 'AIzaSyALjVcdWyjEGOSQYeNVcSfMSH8Ad8skQuY',
-    cx: '011311593262593075698:yu5va13zddu'
-});
-
-//Watson API
-const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-    version: '2018-03-16',
-    iam_apikey: 'tOoqgWF90q1hT_RIsm_HjKrLjORmLIkdDSkj47baGwnR',
-    url: 'https://gateway-syd.watsonplatform.net/natural-language-understanding/api'
-});
-
-
-function getURLsFromGoogle(query) {
+function getDBpediaInfo(value) {
     return new Promise((resolve, reject) => {
+        const query = `
+        select ?n,?abode  where {
+            ?uri dbp:name ?n;
+                dbp:godOf ?go;
+                dbp:type ?t;
+                dbp:abode ?a. 
+                ?a rdfs:label ?abode.
+            
+            Filter(regex(?t,".*Greek.*") and regex(?n,".*${value}( |$)") and lang(?abode)='en')
+        } `
+        
+        fs.writeFile("./queries/sparqlQueries/sparqlQuery.txt", query, function(err) {
+            if(err) {
+                throw (err);
+            }
+        
+            console.log("The file was saved!");
+        }); 
+        
+        const testscript = exec('SPARQL_QUERY_PATH=./queries/sparqlQueries/sparqlQuery.txt ./queries/dbpediaSPARQL.sh');
 
-        googleSearch.build({
-            q: query,
-            num: 10, // Number of search results to return between 1 and 10, inclusive
-            //siteSearch: "http://kitaplar.ankara.edu.tr/" // Restricts results to URLs from a specified site
-            }, function(error, urls) {
-                if (error) throw error
-                resolve(urls)
+        testscript.stdout.on('data', function(data, err){
+            if (err) throw (err);
+
+
+            //clean file
+            fs.truncate('./queries/sparqlQueries/sparqlQuery.txt', 0, function(){console.log('cleaning done')});
+
+            resolve(JSON.parse(data))
         });
     }
 )}
 
-function getURIsFromWatson(url) { 
-    var res = "";
-    console.log("url",url); 
-    const parameters = {
-        'url': url,
-        'features': {
-          'concepts': {
-            'limit': 3
-          }
-        }
-    };
-    naturalLanguageUnderstanding.analyze(parameters, function(error, response) {
-        //if (error) console.log(error)
-        //console.log("response", response)
-        if(response !== null)  res = JSON.stringify(response, null, 2);
-        console.log("res", res)
-    });
 
-    return res;
-}
-
-module.exports = {getURLsFromGoogle, getURIsFromWatson}
+module.exports = {getDBpediaInfo}
--- back/callAPI.js
+++ back/callAPI.js
@@ -4,21 +4,12 @@ const api = require('./api')
 
 module.exports = (app, options) => {
     
-    app.get('/url/:query', (req, res, next) => {
-        console.log("query", req.params.query)
+    app.get('/url/:value', (req, res, next) => {
+        console.log("query", req.params.value)
         //récupération des URLs
-        api.getURLsFromGoogle(req.params.query).then(response => {
-            //récupération des URIs correspondant à chaque URL
-            //console.log(response)
-            //var jsonArray = []
-            var url
-            for (var i = 0; i < response.items.length; i++) {
-                url = response.items[i].formattedUrl;
-                //jsonArray['uri' + i + 1] = api.getURIsFromWatson(url);
-                api.getURIsFromWatson(url);
-                //console.log(jsonArray['uri' + i + 1]);
-            }
-            res.status(200).send('ok')
+        api.getDBpediaInfo(req.params.value).then(response => {
+            res.status(200).json(response)
+            console.log("response", response)
         }).catch(next)
     })
 }
