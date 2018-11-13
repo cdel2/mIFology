067836cf78b9d9200a05da9b418e202576662811
cdel2 <cdelfossecsi@gmail.com>
2018-11-13 13:56:33 +0100

    changed path

--- back/api.js
+++ back/api.js
@@ -14,9 +14,10 @@ function getDBpediaInfo(value) {
             Filter(regex(?t,".*Greek.*") and regex(?n,".*${value}( |$)") and lang(?abode)='en')
         } `;
         
-        const path = "./queries/sparqlQueries/query.txt";
+        const queryPath = "./back/queries/sparqlQueries/query.txt";
+        const scriptPath = "./back/queries/callQuery.sh";
 
-        fs.writeFile(path , query, "utf8", function(err) {
+        fs.writeFile(queryPath , query, "utf8", function(err) {
             if(err) {
                 return console.log("err1", err);
             }
@@ -24,14 +25,17 @@ function getDBpediaInfo(value) {
             console.log("The file was saved!");
         }); 
         
-        const testscript = exec(`SPARQL_QUERY_PATH=${path} ./queries/dbpediaSPARQL.sh`);
+        const testscript = exec(`SPARQL_QUERY_PATH=${queryPath} ${scriptPath}`, function(e, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+            if (e) console.log(e);
+        });
 
         testscript.stdout.on('data', function(data, err){
-            if (err) console.log("err2 =====", err);
-
+            if (err) console.log("err2", err);
 
             //clean file
-            fs.truncate(path, 0, function(){console.log('cleaning done')});
+            fs.truncate(queryPath, 0, function(){console.log('cleaning done')});
 
             resolve(JSON.parse(data))
         });
---
+++ back/dbpediaSPARQL.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+: ${SPARQL_QUERY_PATH?"Please precise SPARQL_QUERY file path by adding SPARQL_QUERY=your_sparql_query_path"}
+OUTPUT_FORMAT=${OUTPUT_FORMAT:-json}
+
+curl -H "Accept: application/$OUTPUT_FORMAT" -g --data-urlencode query@$SPARQL_QUERY_PATH http://dbpedia.org/sparql 2>/dev/null |\
+jq '.["results"]["bindings"]'
