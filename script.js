5b034299876a10258cbb4dba55084a92ccc3cb5c
ymoutaouakil <youssef.moutaouakiloudghiri@amaris.com>
2018-11-16 11:03:59 +0100

    script updated 1

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var godsArray = []
 
-function getGodsInfo(){
+function getGodsInfo() {
+
+    //Transform the name of the god with the good capital letters
+
+    var godNamewithGoodCaps= ($("#GodName").val().charAt(0)).toUpperCase();
+    
+    for (var i = 1; i < $("#GodName").val().length; i++) {
+            godNamewithGoodCaps += ($("#GodName").val().charAt(i)).toLowerCase();
+           
+    }
+
     console.log(godsArray)
-    console.log($("#GodName").val())
-    console.log(godsArray.indexOf($("#GodName").val()))
-    if (godsArray.indexOf($("#GodName").val()) !== -1) {
+    if (godsArray.indexOf(godNamewithGoodCaps) !== -1) {
         document.getElementById("resultTable").style.display="block";
         document.getElementById("noGodFound").style.display="none";
     }
-    if (godsArray.indexOf($("#GodName").val()) === -1) {
+    if (godsArray.indexOf(godNamewithGoodCaps) === -1) {
         document.getElementById("resultTable").style.display="none";
         document.getElementById("noGodFound").style.display="block";
     }
@@ -22,33 +30,44 @@ function getGodsInfo(){
     var parents = []
     var children = []
     var consorts = []
+    var games = []
     var URL = 'https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=';
     var suffix = '&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+';
+    var gQuery = `select%20%3Furi%20as%20%3Fresource%2C%20STR(%3Fn)%20as%20%3FnameOfGod%2C%20%3Fimage%2C%20STR(%3Fgo)%20as%20%3FGodOf%2CSTR(%3Fabode)%20as%20%3FAbode%2C%20if(EXISTS%7B%3Furi%20foaf%3Agender%20%3Fge%7D%2C%20STR(%3Fge)%2C%20(if(regex(%3FGodOf%2C%22.*God%20.*%22)%2C%20%22Male%22%2C%22Female%22)))%20as%20%3FGender%20where%20%7B%0A%0A%20%20%20%3Furi%20dbp%3Aname%20%3Fn%3B%0A%20%20%20dbp%3Atype%20%3Ft.%0A%20%20%20Filter(regex(%3Ft%2C%22.*Greek.*%22)%20and%20datatype(%3Fn)%3Drdf%3AlangString%20and%20regex(%3Fn%2C%20%22.*` + godNamewithGoodCaps + `(_%7C%20%7C%24)%22))%0A%20%20%20%7B%0A%20%20%20%20%3Furi%20dbp%3AgodOf%20%3Fgo.%0A%20%20%20%20FILTER(isLiteral(%3Fgo))%0A%20%20%20%7D%0A%20%20%20UNION%0A%20%20%20%7B%0A%20%20%20%20%3Furi%20dbp%3AgodOf%20%3Fgoresource.%0A%20%20%20%20%3Fgoresource%20rdfs%3Alabel%20%3Fgo.%0A%20%20%20%20FILTER(isLiteral(%3Fgo)%20and%20lang(%3Fgo)%3D%22en%22)%0A%20%20%20%7D%0A%20%20%20optional%7B%3Furi%20dbo%3Athumbnail%20%3Fimage%7D.%0A%0A%20%20%20optional%7B%0A%20%20%20%20%20%20%3Furi%20dbp%3Aabode%20%3Fa.%20%0A%20%20%20%20%20%20%3Fa%20rdfs%3Alabel%20%3Fabode.%0A%20%20%20%20%20%20FILTER(lang(%3Fabode)%3D%22en%22)%0A%20%20%20%7D.%0A%20%20%20optional%7B%3Furi%20foaf%3Agender%20%3Fge%7D.%0A%7D`
     var generalQuery = `
-        select ?uri as ?resource, STR(?n) as ?nameOfGod, ?image, STR(?go) as ?GodOf,STR(?abode) as ?Abode, if(bound(?ge), STR(?ge), if(regex(?go,"God .*") , "Male","Female")) as ?Gender where {
-        ?uri dbp:name ?n;
-        dbp:godOf ?go;
-        dbp:type ?t;
-        dbp:godOf ?go.
+        select ?uri as ?resource, STR(?n) as ?nameOfGod, ?image, STR(?go) as ?GodOf, STR(?abode) as ?Abode, if(EXISTS{?uri foaf: gender ?ge}, STR(?ge), (if(regex(?GodOf, ".*God .*"), "Male", "Female"))) as ?Gender where {
 
-        optional{?uri dbo:thumbnail ?image}.
+   ?uri dbp: name ?n;
+        dbp: type ?t.
+   Filter(regex(?t, ".*Greek.*") and datatype(?n) =rdf: langString and regex(?n, ".*`+godNamewithGoodCaps+`(_| |$)"))
+    {
+    ?uri dbp: godOf ?go.
+    FILTER(isLiteral(?go))
+    }
+   UNION
+    {
+    ?uri dbp: godOf ?goresource.
+    ?goresource rdfs: label ?go.
+    FILTER(isLiteral(?go) and lang(?go) ="en")
+    }
+   optional{?uri dbo: thumbnail ?image}.
 
-        optional{
-        ?uri dbp:abode ?a. 
-        ?a rdfs:label ?abode.
-        FILTER(lang(?abode)="en")}.
+   optional{
+      ?uri dbp: abode ?a.
+      ?a rdfs: label ?abode.
+      FILTER(lang(?abode) ="en")
+    }.
+   optional{?uri foaf: gender ?ge}.
+    }
 
-        optional{?uri foaf:gender ?ge}.
-
-        Filter(regex(?t,".*Greek.*") and regex(?n,".*`+$("#GodName").val()+`( |$)","i"))}
-        `
+    `
     var siblingsQuery = `
         SELECT DISTINCT STR(?sibling) as ?Sibling
         WHERE {
         ?uri dbp:name ?n;
         dbp:godOf ?go;
         dbp:type ?t.
-        FILTER(regex(?t,".*Greek.*") and regex(?n,".*`+$("#GodName").val()+`( |$)","i"))
+        FILTER(regex(?t,".*Greek.*") and regex(?n,".*`+godNamewithGoodCaps+`( |$)","i"))
 
         {
         VALUES ?N { 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30} 
@@ -88,7 +107,7 @@ function getGodsInfo(){
             FILTER(isLiteral(?symbol))
         }
     
-        Filter(regex(?t,".*Greek.*") and regex(?n,".*`+$("#GodName").val()+`( |$)","i"))
+        Filter(regex(?t,".*Greek.*") and regex(?n,".*`+godNamewithGoodCaps+`( |$)","i"))
         }`
 
     var childrenQuery = `
@@ -97,7 +116,7 @@ function getGodsInfo(){
         ?uri dbp:name ?n;
         dbp:godOf ?go;
         dbp:type ?t.
-        FILTER(regex(?t,".*Greek.*") and regex(?n,".*`+$("#GodName").val()+`( |$)","i"))
+        FILTER(regex(?t,".*Greek.*") and regex(?n,".*`+godNamewithGoodCaps+`( |$)","i"))
         
         {
             VALUES ?N { 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30} 
@@ -126,7 +145,7 @@ function getGodsInfo(){
         ?uri dbp:name ?child;
         dbp:godOf ?go;
         dbp:type ?t.
-        Filter(regex(?t,".*Greek.*") and regex(?child,".*`+$("#GodName").val()+`( |$)","i"))
+        Filter(regex(?t,".*Greek.*") and regex(?child,".*`+godNamewithGoodCaps+`( |$)","i"))
         {
             ?uri dbp:parents ?parents.
             FILTER(!isBlank(?parents) and isLiteral(?parents))
@@ -149,7 +168,7 @@ function getGodsInfo(){
             ?uri dbp:name ?n;
             dbp:godOf ?go;
             dbp:type ?t.
-            FILTER(regex(?t,".*Greek.*") and regex(?n,".*`+$("#GodName").val()+`( |$)","i"))
+            FILTER(regex(?t,".*Greek.*") and regex(?n,".*`+godNamewithGoodCaps+`( |$)","i"))
             ?consortRes dbp:consort ?ourGod;
             dbp:type ?t.
             FILTER(!isBlank(?ourGod) and isLiteral(?ourGod))
@@ -165,7 +184,7 @@ function getGodsInfo(){
             ?uri dbp:name ?n;
             dbp:godOf ?go;
             dbp:type ?t.
-            FILTER(regex(?t,".*Greek.*") and regex(?n,".*`+$("#GodName").val()+`( |$)","i"))
+            FILTER(regex(?t,".*Greek.*") and regex(?n,".*`+godNamewithGoodCaps+`( |$)","i"))
             ?consortRes dbp:consort ?ourGod;
             dbp:type ?t.   
             ?ourGod dbp:type ?t;
@@ -180,7 +199,7 @@ function getGodsInfo(){
         ?uri dbp:name ?n;
         dbp:godOf ?go;
         dbp:type ?t.
-        Filter(regex(?t,".*Greek.*") and regex(?n,".*`+$("#GodName").val()+`( |$)"))
+        Filter(regex(?t,".*Greek.*") and regex(?n,".*`+godNamewithGoodCaps+`( |$)"))
 
         {
             VALUES ?N { 1 2 3 4} 
@@ -198,36 +217,70 @@ function getGodsInfo(){
         }
         }
     `
-    var encodedGeneralQuery = URL+encodeURI(generalQuery)+suffix
+    var gamesQuery = `
+        select STR(?label) as ?game where {
+
+        ?uri ?b dbo:VideoGame.
+        ?uri rdfs:comment ?comment.
+        ?uri dbo:abstract ?abstract.
+        ?uri rdfs:label ?label.
+         Filter( lang(?label)="en" and ( lang(?comment)="en" and lang(?abstract)="en") and ( regex(?comment," `+godNamewithGoodCaps+`( |,|.)") || regex(?abstract," `+godNamewithGoodCaps+`( |,|.)") ) )
+
+        }
+        `
+    var encodedGeneralQuery = URL + gQuery + suffix
     var encodedSiblingsQuery = URL+encodeURI(siblingsQuery)+suffix
     var encodedSymbolsQuery = URL+encodeURI(symbolsQuery)+suffix
     var encodedChildrenQuery = URL+encodeURI(childrenQuery)+suffix
     var encodedParentsQuery = URL+encodeURI(parentsQuery)+suffix
-    var encodedConsortsQuery = URL+encodeURI(consortsQuery)+suffix
-    var encodedConsortsQuery2 = URL+encodeURI(consortsQuery2)+suffix
+    var encodedConsortsQuery = URL + encodeURI(consortsQuery) + suffix
+    var encodedConsortsQuery2 = URL + encodeURI(consortsQuery2) + suffix
+    var encodedGamesQuery = URL + encodeURI(gamesQuery) + suffix
 
     $.ajax({ 
         url: encodedGeneralQuery, 
         success: function(result) {
-            var results = result.results.bindings; 
+            var results = result.results.bindings;
             for (var res in results) {
-                tmpAbode = results[res].Abode.value;   
-                if (abode.indexOf(" " + tmpAbode) === -1) abode.push(" " + tmpAbode)
-                tmpGender = results[res].Gender.value;
-                if (gender.indexOf(tmpGender) === -1) gender.push(tmpGender)
-                tmpGodOf = results[res].GodOf.value;
-                if (godOf.indexOf(" " + tmpGodOf) === -1) godOf.push(" " + tmpGodOf)
-                tmpImage = results[res].image.value;
-                if (image.indexOf(tmpImage) === -1) image.push(tmpImage)
-                tmpNameOfGod = results[res].nameOfGod.value;
-                if (nameOfGod.indexOf(tmpNameOfGod) === -1) nameOfGod.push(tmpNameOfGod)
+                if (typeof (results[res].Abode) !== 'undefined') {
+                    tmpAbode = results[res].Abode.value;
+                    if (abode.indexOf(" " + tmpAbode) === -1) abode.push(" " + tmpAbode)
+                }
+                if (typeof (results[res].Gender) !== 'undefined') {
+                    tmpGender = results[res].Gender.value;
+                    if (gender.indexOf(tmpGender) === -1) gender.push(tmpGender)
+                }
+                if (typeof (results[res].GodOf) !== 'undefined') {
+                    tmpGodOf = results[res].GodOf.value;
+                    if (godOf.indexOf(" " + tmpGodOf) === -1) godOf.push(" " + tmpGodOf)
+                }
+                if (typeof (results[res].image) !== 'undefined') {
+                    tmpImage = results[res].image.value;
+                    if (image.indexOf(tmpImage) === -1) image.push(tmpImage)
+                }
+                if (typeof (results[res].nameOfGod) !== 'undefined') {
+                    tmpNameOfGod = results[res].nameOfGod.value;
+                    if (nameOfGod.indexOf(tmpNameOfGod) === -1) nameOfGod.push(tmpNameOfGod)
+                }
             }
-            document.getElementById('godName').innerHTML=$("#GodName").val();
-            document.getElementById('godFunction').innerHTML=godOf;
-            document.getElementById('godGender').innerHTML=gender;
-            document.getElementById('godImage').src=image;
-            document.getElementById('godImage').alt=$("#GodName").val();
-            document.getElementById('godAbode').innerHTML=abode;
+
+            if (nameOfGod.length == 0) document.getElementById('godName').innerHTML = "No name found";
+            else document.getElementById('godName').innerHTML = godNamewithGoodCaps;
+
+            if (godOf.length == 0) document.getElementById('godFunction').innerHTML = "No function found";
+            else document.getElementById('godFunction').innerHTML = godOf;
+
+            if (gender.length == 0) document.getElementById('godGender').innerHTML = "No Gender found";
+            else document.getElementById('godGender').innerHTML = gender;
+
+            if (image.length == 0) document.getElementById('godImage').src = "imageNotFound.png"
+            else {
+                document.getElementById('godImage').src = image;
+                document.getElementById('godImage').alt = godNamewithGoodCaps;
+            }
+
+            if (abode.length == 0) document.getElementById('godAbode').innerHTML = "No abode found";
+            else document.getElementById('godAbode').innerHTML=abode;
         } 
     }); 
         
@@ -239,7 +292,8 @@ function getGodsInfo(){
                 sibling = results[res].Sibling.value
                 if (siblings.indexOf(" " + sibling) === -1) siblings.push(" " + sibling)
             }
-            document.getElementById('godSiblings').innerHTML=siblings;
+            if (siblings.length == 0) document.getElementById('godSiblings').innerHTML = "No siblings found";
+            else document.getElementById('godSiblings').innerHTML=siblings;
         } 
     }); 
 
@@ -251,7 +305,8 @@ function getGodsInfo(){
                 symbol = results[res].Symbol.value
                 if (symbols.indexOf(" " + symbol) === -1) symbols.push(" " + symbol)
             }
-            document.getElementById('godSymbol').innerHTML=symbols;
+            if (symbols.length == 0) document.getElementById('godSymbol').innerHTML = "No symbols found";
+            else document.getElementById('godSymbol').innerHTML=symbols;
         } 
     }); 
         
@@ -263,7 +318,8 @@ function getGodsInfo(){
                 child = results[res].Children.value
                 if (children.indexOf(" " + child) === -1) children.push(" " + child)
             }
-            document.getElementById('godChildren').innerHTML=children;
+            if (children.length == 0) document.getElementById('godChildren').innerHTML = "No children found";
+            else document.getElementById('godChildren').innerHTML=children;
         } 
     }); 
         
@@ -275,28 +331,43 @@ function getGodsInfo(){
                 parent = results[res].parent.value
                 if (parents.indexOf(" " + parent) === -1) parents.push(" " + parent)
             }
-            document.getElementById('godParents').innerHTML=parents;
+            if (parents.length == 0) document.getElementById('godParents').innerHTML = "No parents found";
+            else document.getElementById('godParents').innerHTML=parents;
         } 
     }); 
         
-    $.ajax({ 
-        url: encodedConsortsQuery, 
-        success: function(result) {
-            var results = result.results.bindings; 
+    $.ajax({
+        url: encodedConsortsQuery,
+        success: function (result) {
+            var results = result.results.bindings;
             for (var res in results) {
                 consort = results[res].Consorts.value
                 if (consorts.indexOf(" " + consort) === -1) consorts.push(" " + consort)
             }
-        } 
-    }).then($.ajax({ 
-        url: encodedConsortsQuery2, 
-        success: function(result) {
-            var results = result.results.bindings; 
+        }
+    }).then($.ajax({
+        url: encodedConsortsQuery2,
+        success: function (result) {
+            var results = result.results.bindings;
             for (var res in results) {
                 consort = results[res].Consort.value
                 if (consorts.indexOf(" " + consort) === -1) consorts.push(" " + consort)
             }
-            document.getElementById('godConsorts').innerHTML=consorts;
-        } 
-    }))
+            if (consorts.length == 0) document.getElementById('godConsorts').innerHTML = "No consorts found";
+            else document.getElementById('godConsorts').innerHTML = consorts;
+        }
+    }));
+
+    $.ajax({
+        url: encodedGamesQuery,
+        success: function (result) {
+            var results = result.results.bindings;
+            for (var res in results) {
+                game = results[res].game.value
+                if (games.indexOf(" " + game) === -1) games.push(" " + game)
+            }
+            if (games.length == 0) document.getElementById('godGames').innerHTML = "No games found";
+           else document.getElementById('godGames').innerHTML = games;
+        }
+    });
 }
